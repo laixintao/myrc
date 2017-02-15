180e8ae6e1bbd7f5712b0599c18393c7e8d3de42
laixintao <laixintao1995@163.com>
2017-02-15 15:48:02 +0800

    add webpack to tmux. add select-layout to tmux

--- dem.sh
+++ dem.sh
@@ -4,7 +4,8 @@ tmux new-session -s demlution -n vim -d
 
 # target [session]:[window].[pane]
 tmux new-window -n panel -t demlution
-tmux split-window -h -t demlution:2 # panel in right，for gulp 1.2
+tmux split-window -h -t demlution:2 # gulp 1.2
+tmux split-window -v -t demlution:2 # webpack
 tmux split-window -v -t demlution:2 # django，1.3
 tmux split-window -v -t demlution:2 # shadow socks，1.4
 
@@ -16,13 +17,14 @@ tmux send-keys -t demlution:1.1 'vim' C-m
 tmux send-keys -t demlution:2.1 'cd ~/program/bazaar4' C-m
 tmux send-keys -t demlution:2.1 'workon bz' C-m
 tmux send-keys -t demlution:2.2 'cd ~/program/bazaar4' C-m
-tmux send-keys -t demlution:2.2 'gulp' C-m
-
+tmux send-keys -t demlution:2.2 'npm run dmhome' C-m
 tmux send-keys -t demlution:2.3 'cd ~/program/bazaar4' C-m
-tmux send-keys -t demlution:2.3 'workon bz' C-m
-tmux send-keys -t demlution:2.3 'python -Wi manage.py runserver' C-m
-tmux send-keys -t demlution:2.4 'dem-sslocal' C-m
-
+tmux send-keys -t demlution:2.3 'gulp' C-m
+tmux send-keys -t demlution:2.4 'cd ~/program/bazaar4' C-m
+tmux send-keys -t demlution:2.4 'workon bz' C-m
+tmux send-keys -t demlution:2.4 'python -Wi manage.py runserver' C-m
+tmux send-keys -t demlution:2.5 'dem-sslocal' C-m
+tmux select-layout -t demlution:2.1 main-vertical 
 
 # enter tmux, select vim window
 tmux select-window -t demlution:1
