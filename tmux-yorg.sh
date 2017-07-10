9bdfc0a3cd7de5d14c49d5c44eaacc6b00982624
laixintao <laixintao1995@163.com>
2017-07-10 10:14:35 +0800

    change yorg-contro brm-index env to yorg
    single env is enough(save disk)

--- tmux-yorg.sh
+++ tmux-yorg.sh
@@ -26,14 +26,14 @@ tmux split-window -h -t yorg-dev:3
 
 tmux send-keys -t yorg-dev:3.1 'cd ../yorg-control/' C-m
 tmux send-keys -t yorg-dev:3.2 'cd ../yorg-control/' C-m
-tmux send-keys -t yorg-dev:3.1 'workon ycontrol' C-m
-tmux send-keys -t yorg-dev:3.2 'workon ycontrol' C-m
+tmux send-keys -t yorg-dev:3.1 'workon yorg' C-m
+tmux send-keys -t yorg-dev:3.2 'workon yorg' C-m
 
 # yorg-control
 tmux new-window -n brm-index -t yorg-dev
 tmux split-window -h -t yorg-dev:4
-tmux send-keys -t yorg-dev:4.1 'workon brm-index' C-m
-tmux send-keys -t yorg-dev:4.2 'workon brm-index' C-m
+tmux send-keys -t yorg-dev:4.1 'workon yorg' C-m
+tmux send-keys -t yorg-dev:4.2 'workon yorg' C-m
 tmux send-keys -t yorg-dev:4.1 'cd ../brm-index/' C-m
 tmux send-keys -t yorg-dev:4.2 'cd ../brm-index/' C-m
 tmux send-keys -t yorg-dev:4.1 'source .env.testing' C-m
@@ -48,6 +48,6 @@ tmux split-window -v -t yorg-dev:5 # 2.3 redis
 tmux split-window -v -t yorg-dev:5 # 2.4 sslocal
 tmux send-keys -t yorg-dev:5.3 'cd ~' C-m
 tmux send-keys -t yorg-dev:5.3 'redis-server;if [ $? -eq 0 ];then echo success;else exit;fi' C-m
-tmux send-keys -t yorg-dev:5.4 'sslocal -c /etc/shadowsocks.json' C-m
+tmux send-keys -t yorg-dev:5.4 'mongod' C-m
 
 tmux -2 attach -t yorg-dev:1
