a770f3a9ce0c0787875b92898fa13e16b248fd3e
laixintao <laixintaoo@gmail.com>
2020-03-28 17:59:59 +0800

    convert: scirpt to convert trojan format to clash config.

--- bin/convert_ss_server.py
+++ bin/convert_ss_server.py
@@ -1,13 +1,20 @@
 #!/usr/local/var/pyenv/shims/python3
 # -*- coding: utf-8 -*-
 
+import sys
 import json
 
-config = json.load(open("gui-config.json"))
-for ss in config["configs"]:
-    print(
-        f"- {{ name: \"{ss['remarks']}\", type: ss, server: {ss['server']}, port: {ss['server_port']}, cipher: {ss['method']}, password: {ss['password']} }}"
-    )
 
-for ss in config["configs"]:
-    print(f"- \"{ss['remarks']}\"")
+def convert_ss_to_clash_config(filename):
+    config = json.load(open(filename))
+    for ss in config["configs"]:
+        print(
+            f"- {{ name: \"{ss['remarks']}\", type: ss, server: {ss['server']}, port: {ss['server_port']}, cipher: {ss['method']}, password: {ss['password']} }}"
+        )
+
+    for ss in config["configs"]:
+        print(f"- \"{ss['remarks']}\"")
+
+
+if __name__ == "__main__":
+    convert_ss_to_clash_config(sys.argv[1])
