d63863df601105eef10c13356dcc3256b159c0ae
laixintao <laixintaoo@gmail.com>
2020-04-02 10:32:11 +0800

    clash&pgcli: change pgcli's prompt, update clash status.

--- bin/clash-status.sh
+++ bin/clash-status.sh
@@ -1,4 +1,4 @@
-select_server=$(tac /tmp/clash.log | tac /tmp/clash.log | rg -e "\[(.*)\]" -m 1 -o -r '$1')
+select_server=$(tac /tmp/clash.log | tac /tmp/clash.log | rg -e "Proxy\[(.*)\]" -m 1 -o -r '$1')
 jq_query=.proxies.\"$select_server\".history[-1].delay
 server_delay=$(curl -s "localhost:9090/proxies" | jq --raw-output "$jq_query")
 
