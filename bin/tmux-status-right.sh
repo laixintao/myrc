157c36936060223155a83ea03aa4386390f8be1f
laixintao <laixintaoo@gmail.com>
2024-11-06 10:16:45 +0800

    add cpu freq on right status.

--- bin/tmux-status-right.sh
+++ bin/tmux-status-right.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cpu_freq=$(sudo powermetrics -i 1000 -n 1 -s cpu_power \
+    | grep "System Average frequency as fraction of nominal" \
+    | awk '{sum += substr($8, 1, length($8)-1)} END {print sum/NR}')
 
 load1=$(uptime | rg -o "load average: (.*?)," -r "\$1")
 
 load1round=$( printf "%.0f" $load1 )
 
-echo $load1round
+echo " $load1round / $cpu_freq%"
