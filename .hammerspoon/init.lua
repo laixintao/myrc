f37028baf120a290da015d95f066cf5584881ae9
laixintao <laixintaoo@gmail.com>
2026-04-20 10:32:40 +0800

    quick seatalk and roam

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -25,3 +25,29 @@ updateHotkey()
 hs.hotkey.bind({"cmd"}, "G", function()
     hs.application.launchOrFocus("ChatGPT")
 end)
+
+hs.hotkey.bind({"cmd"}, "3", function()
+    local appName = "SeaTalk"
+    local app = hs.application.get(appName)
+
+    if app and app:isFrontmost() then
+        -- 如果当前就在前台 → 隐藏
+        app:hide()
+    else
+        -- 否则 → 打开或切换到前台
+        hs.application.launchOrFocus(appName)
+    end
+end)
+
+hs.hotkey.bind({"cmd"}, "4", function()
+    local appName = "Roam Research"
+    local app = hs.application.get(appName)
+
+    if app and app:isFrontmost() then
+        -- 如果当前就在前台 → 隐藏
+        app:hide()
+    else
+        -- 否则 → 打开或切换到前台
+        hs.application.launchOrFocus(appName)
+    end
+end)
